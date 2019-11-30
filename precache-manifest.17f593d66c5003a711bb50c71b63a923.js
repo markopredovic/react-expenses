fdf5af417019d952b74addb1d5373b5abe573bf8
Marko Predovic <mpredovic@gmail.com>
2019-11-30 12:18:51 +0100

    Updates

---
+++ precache-manifest.17f593d66c5003a711bb50c71b63a923.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16ae59dcb13a2483b179a5345d69f3ed",
+    "url": "/react-expenses/index.html"
+  },
+  {
+    "revision": "4e3e019ef0ef9adb37a2",
+    "url": "/react-expenses/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "27a0640938e04ed3353b",
+    "url": "/react-expenses/static/css/main.c2f48785.chunk.css"
+  },
+  {
+    "revision": "4e3e019ef0ef9adb37a2",
+    "url": "/react-expenses/static/js/2.9ef0b40f.chunk.js"
+  },
+  {
+    "revision": "27a0640938e04ed3353b",
+    "url": "/react-expenses/static/js/main.6cd32efe.chunk.js"
+  },
+  {
+    "revision": "8f953c05e4fdadacef9c",
+    "url": "/react-expenses/static/js/runtime-main.465765a2.js"
+  }
+]);
