8f60be821a820aed327d4b9b88179095ef8cdf1b
Marko Predovic <mpredovic@gmail.com>
2019-12-04 15:11:20 +0100

    clear console.log

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import ExpensesContext from "../../context/ExpensesContext";
 import LocalizedStrings from "react-localization";
 
@@ -13,26 +13,10 @@ const Footer = () => {
   });
 
   const context = useContext(ExpensesContext);
+
   if (context.state) {
     strings.setLanguage(context.state.lang);
   }
-
-  console.log("[FOOTER RENDER]");
-
-  useEffect(() => {
-    console.log("[FOOTER USE EFFECT]");
-    if (context.state) {
-      strings.setLanguage(context.state.lang);
-    }
-  });
-
-  useEffect(() => {
-    console.log("[FOOTER USE EFFECT 2]");
-    if (context.state) {
-      strings.setLanguage(context.state.lang);
-      console.log("GET LANGUAGE", strings.getLanguage());
-    }
-  }, [context.state]);
 
   return (
     <footer>
