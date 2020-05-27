37e10ae18e653effd95a7030bda7cb396f6ed8ed
Marko Predovic <mpredovic@gmail.com>
2020-05-27 07:05:54 +0200

    env

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import store from "./store";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:1337",
+  baseURL: process.env.REACT_APP_STRAPI_BASE_URL,
   timeout: 1000,
 });
 
@@ -27,23 +27,4 @@ const addAuthorizationHeader = () => {
   );
 };
 
-const removeAuthorizationHeader = () => {
-  axiosInstance.interceptors.request.use(
-    function (config) {
-      config.headers = {
-        ...config.headers,
-        Authorization: "",
-      };
-      return config;
-    },
-    function (error) {
-      return Promise.reject(error);
-    }
-  );
-};
-
-export {
-  axiosInstance as default,
-  addAuthorizationHeader,
-  removeAuthorizationHeader,
-};
+export { axiosInstance as default, addAuthorizationHeader };
